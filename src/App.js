bf3b8bfd8c67bc81a866e9dec859eae936b873c9
JC1201 <jiehchen@lancaster.ac.uk>
2025-08-26 23:18:50 +0100

    feat: integrate Firebase authentication and event management
    - Added Firebase as a dependency in package.json.
    - Implemented user authentication with email and password in Auth component.
    - Created Event component to manage and display events shared with the authenticated user.
    - Set up Firestore to store event data and Firebase Storage for image uploads.
    - Established Firebase configuration in a new firebase.js file.
    - Updated App component to conditionally render Auth or Event based on user authentication state.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Auth from "./components/Auth";
+import Event from "./components/Event";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!user ? <Auth setUser={setUser} /> : <Event user={user} />}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxdLoPrfSwz_nF6javwGtFomxcC3sbo3s",
+  authDomain: "day-counter-8b702.firebaseapp.com",
+  projectId: "day-counter-8b702",
+  storageBucket: "day-counter-8b702.firebasestorage.app",
+  messagingSenderId: "553691624006",
+  appId: "1:553691624006:web:62e0b611ad8810f096d047",
+  measurementId: "G-NG9RF3DBPF"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
